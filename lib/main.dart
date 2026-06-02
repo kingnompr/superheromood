d3d0ba91359f9e89beec9c18cd1edb9f37a1d734
Nomensen <nomensenpardosi@gmail.com>
2026-06-02 10:51:48 +0700

    Initial project structure

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:superheromood/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Superhero Mood',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: MainScreen.id,
+      routes: {
+        MainScreen.id: (context) => const MainScreen(),
+      },
+    );
+  }
+}
